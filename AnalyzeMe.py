bd7f90e8a92bf3356d4e0f11ca3df842ce640fd8
David Gardner <davjgardner@gmail.com>
2019-06-27 21:35:23 -0700

    move to unified CLI using argparse

--- AnalyzeMe.py
+++ AnalyzeMe.py
@@ -13,40 +13,38 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # print messages to file in a human-readable format
-def humanReadable(data, argv):
-    outfname = argv[3] if len(argv) >=4 else 'out.txt'
-    outfile = open(outfname, "w")
+def humanReadable(args):
+    data = json.load(args.data)
     for message in reversed(data):
         name = message["name"] if message["name"] != None else ""
         liked = "[<3]" if len(message["favorited_by"]) > 0 else ""
         time = datetime.fromtimestamp(message['created_at'], tz=tz.tzlocal())
         #attachment = "[" + message["attachments"][0] + "]" if len(message["attachments"]) > 0 else ""
         text = message["text"] if message["text"] != None else ""
-        outfile.write("[{}/{:02d}/{:02d} {:02d}:{:02d}] {} {}: {}\n".format(time.year, time.month, time.day,
-                                                            time.hour, time.minute, name, liked, text))
-    return "Written output to {}".format(outfname)
+        args.output.write("[{}/{:02d}/{:02d} {:02d}:{:02d}] {} {}: {}\n".format(time.year, time.month, time.day,time.hour, time.minute, name, liked, text))
 
 # return a dictionary of username->number of messages
-def messageCount(data, argv):
+def messageCount(args):
+    data = json.load(args.data)
     users = {}
     for message in data:
         users[message['name']] = users.get(message['name'], 0) + 1
+    if args.plot:
+        fig1, ax1 = plt.subplots()
+        fig1.suptitle('Total message count')
+        def pfmt(pct, values):
+            return '{:.1f}% ({})'.format(pct, int(round(pct * sum(values) / 100.0)))
+        wedges, texts, autotexts = ax1.pie(users.values(), labels=users.keys(),
+                                           autopct=lambda pct:pfmt(pct, users.values()))
+        ax1.axis('equal')
+        plt.show()
+    else:
+        list(map(lambda u: args.output.write('{}, {}\n'.format(u, users[u])), users.keys()))
     return users
 
-def messageCountPlot(data, argv):
-    counts = messageCount(data, argv)
-    fig1, ax1 = plt.subplots()
-    fig1.suptitle('Total message count')
-    def pfmt(pct, values):
-        return '{:.1f}% ({})'.format(pct, int(round(pct * sum(values) / 100.0)))
-    wedges, texts, autotexts = ax1.pie(counts.values(), labels=counts.keys(),
-                                       autopct=lambda pct:pfmt(pct, counts.values()))
-    ax1.axis('equal')
-    plt.show()
-
-
 # return a dictionary of username->average message length
-def messageLength(data, argv):
+def messageLength(args):
+    data = json.load(args.data)
     lens = {}
     msgs = {}
     for message in data:
@@ -57,97 +55,111 @@ def messageLength(data, argv):
         msgs[message['name']] = msgs.get(message['name'], 0) + 1
     for user in lens:
         lens[user] = lens[user] / msgs[user]
+    if args.plot:
+        print('no plot yet')
+    else:
+        list(map(lambda u: args.output.write('{}, {}\n'.format(u, lens[u])), lens.keys()))
     return lens
 
 # generate a map of username->number of attachments sent
-def attachmentCount(data, argv):
+def attachmentCount(args):
+    data = json.load(args.data)
     attachments = {}
     for message in data:
         a = attachments.get(message['name'], 0)
         attachments[message['name']] = a + len(message['attachments'])
+    if args.plot:
+        print('no plot yet')
+    else:
+        list(map(lambda u: args.output.write('{}, {}\n'.format(u, attachments[u])),
+                 attachments.keys()))
     return attachments
 
 # count how many messages were sent per day by each user
 # produce a map date->{user->count}
-def messagesPerDay(data, argv):
+def messagesPerDay(args):
+    data = json.load(args.data)
     dates = {}
     for message in data:
         day = datetime.fromtimestamp(message['created_at'], tz=tz.tzlocal())
         daystring = "{}/{}/{}".format(day.month, day.day, day.year)
         if not daystring in dates: dates[daystring] = {}
         dates[daystring][message['name']] = dates[daystring].get(message['name'], 0) + 1
+    if args.plot:
+        print('no plot yet')
+    else:
+        # TODO: probably need to pretty print date
+        list(map(lambda d: args.output.write('{}, {}\n'.format(d, dates[d])), dates.keys()))
     return dates
 
 # produce a histogram of how many messages were sent by each user in each hour of the day
 # returns a map of user to histogram of messages per hour
-def hourHistogram(data, argv):
+def hourHistogram(args):
+    data = json.load(args.data)
     hours = {}
     for message in data:
         time = datetime.fromtimestamp(message['created_at'], tz=tz.tzlocal())
         if not message['name'] in hours:
             hours[message['name']] = [0] * 24
         hours[message['name']][time.hour] += 1
+    if args.plot:
+        hists = hours
+        xl = range(24)
+        labels = list(map(lambda h: '{}:00'.format(h), xl))
+        x = np.array(xl)
+        width = 0.9
+        p1 = plt.bar(x, list(hists.values())[0], width)
+        p2 = plt.bar(x, list(hists.values())[1], width, bottom=list(hists.values())[0])
+        plt.ylabel('Message count')
+        plt.title('Message Distribution by Hour')
+        plt.xticks(x, labels, rotation=90)
+        plt.legend((p1[0], p2[0]), list(hists.keys()))
+        plt.show()
+    else:
+        list(map(lambda u: args.output.write('{}, {}\n'.format(u, str(hours[u])[1:-1])), hours))
     return hours
 
-def plotHourHistogram(data, argv):
-    hists = hourHistogram(data, argv)
-    xl = range(24)
-    labels = list(map(lambda h: '{}:00'.format(h), xl))
-    x = np.array(xl)
-    width = 0.9
-
-    p1 = plt.bar(x, list(hists.values())[0], width)
-    p2 = plt.bar(x, list(hists.values())[1], width, bottom=list(hists.values())[0])
-    plt.ylabel('Message count')
-    plt.title('Message Distribution by Hour')
-    plt.xticks(x, labels, rotation=90)
-    plt.legend((p1[0], p2[0]), list(hists.keys()))
-    plt.show()
-
-def error(data, argv):
-    return "Unrecognized command: {}".format(argv[2])
-
-# format: 'name': (func, 'args', 'description'),
-cmds = {
-    'len': (messageLength, '', 'average message length per user'),
-    'attachments': (attachmentCount, '', 'number of attachments per user'),
-    'count': (messageCount, '', 'number of messages per user'),
-    'countplot': (messageCountPlot, '', 'plot number of messages per user'),
-    'readable': (humanReadable, '[outfile]', 'output in a human-readable format'),
-    'perday': (messagesPerDay, '', 'number of messages per day'),
-    'perhour': (hourHistogram, '', 'histogram of number of messages per hour'),
-    'hourplot': (plotHourHistogram, '', 'plot per-hour histogram'),
-    'list': (error, '', 'list commands'),
-}
-
-def listcmds(data, argv):
-    print('Available commands:')
-    list(map(lambda s: print('  {} {}: {}'.format(s, cmds[s][1], cmds[s][2])),
-             cmds.keys()))
-
-cmds['list'] = (listcmds, '', 'list commands')
-
-usage = "Usage: AnalyzeMe.py path/to/message.json command [args]"
-
-#if len(sys.argv) >= 1 and (sys.argv[1] == 'help' or sys.argv[2] == 'help'):
-#    print(usage)
-#    print("Available commands: ")
-#    list(map(lambda k: print('  {} {}'.format(k, cmds[k][1])), cmds.keys()))
-#
-#    exit(0)
-
-
 parser = argparse.ArgumentParser(description='Analyze GroupMe conversations')
-parser.add_argument('data', help='/path/to/message.json', type=argparse.FileType('r'))
-parser.add_argument('command', help='operation to perform')
-parser.add_argument('-o', '--output', help='output file', nargs='?',
-                    type=argparse.FileType('w'), default=sys.stdout)
+
+subparsers = parser.add_subparsers()
+
+base_parser = argparse.ArgumentParser(add_help=False)
+base_parser.add_argument('data', type=argparse.FileType('r'), help='/path/to/message.json')
+base_parser.add_argument('--output', '-o', help='write output to a file',
+                         type=argparse.FileType('w'), default=sys.stdout)
+
+graph_parser = argparse.ArgumentParser(parents=[base_parser], add_help=False)
+graph_parser.add_argument('--plot', '-p', help='plot on a graph', action='store_const', const=True)
+
+len_parser = subparsers.add_parser('len', description='average message length per user',
+                                   parents=[graph_parser])
+len_parser.set_defaults(func=messageLength)
+
+attachments_parser = subparsers.add_parser('attachments',
+                                           description='number of attachments per user',
+                                           parents=[graph_parser])
+attachments_parser.set_defaults(func=attachmentCount)
+
+count_parser = subparsers.add_parser('count',
+                                     description='number of messages per user',
+                                     parents=[graph_parser])
+count_parser.set_defaults(func=messageCount)
+
+perday_parser = subparsers.add_parser('perday',
+                                      description='number of messages per day',
+                                      parents=[graph_parser])
+perday_parser.set_defaults(func=messagesPerDay)
+
+perhour_parser = subparsers.add_parser('perhour',
+                                       description='aggregate number of messages per hour',
+                                       parents=[graph_parser])
+perhour_parser.set_defaults(func=hourHistogram)
+
+readable_parser = subparsers.add_parser('readable',
+                                        description=
+                                        'convert the data to a human-readable format',
+                                        parents=[base_parser])
+readable_parser.set_defaults(func=humanReadable)
 
 args = parser.parse_args()
-
-infile = args.data
-#infile = open(args.data, "r")
-
-data = json.load(infile)
-
-print(cmds.get(args.command, (error, ''))[0](data, sys.argv))
+args.func(args)
